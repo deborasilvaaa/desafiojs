5af21b8a62dd160b42030b89687e1659267df4c7
deborasilvaaaa <deborasilvacardosos021@gmail.com>
2023-05-19 04:40:36 -0300

    desafiojs

---
+++ codigos.js
@@ -0,0 +1,17 @@
+
+function ativarNome(){
+    let nome = 'isso é tudo pessoal';
+    console.log(nome) 
+    document.getElementById('nome').innerHTML = nome
+
+}
+
+function somarNumeros(){
+    let a = 10;
+    let b = 8;
+    let c = a/b;
+
+    console.log(c)
+    
+    document.getElementById('aparecer').innerHTML = c
+}
